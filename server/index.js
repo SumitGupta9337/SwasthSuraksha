c6a071a8b1dc4abdb44067b356efa3437c68be12
Sumit Gupta <sumit.edu123@gmail.com>
2026-02-27 17:39:31 +0530

    commit index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
-const path = require("path");   // ✅ ADD THIS
+// const path = require("path");   // ✅ ADD THIS
 
 const app = express();
 
@@ -14,13 +14,13 @@ app.use(express.json());
 // API routes
 app.use("/", routes);
 
-// ✅ SERVE REACT BUILD
-app.use(express.static(path.join(__dirname, "../dist")));
+// // ✅ SERVE REACT BUILD
+// app.use(express.static(path.join(__dirname, "../dist")));
 
-// ✅ SPA SUPPORT (important for /confirm/:token)
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
-});
+// // ✅ SPA SUPPORT (important for /confirm/:token)
+// app.use((req, res) => {
+//   res.sendFile(path.join(__dirname, "../dist/index.html"));
+// });
 
 
 app.listen(3000, () => console.log("🚑 Server running on port 3000"));
